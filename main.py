c8ddda6dfa6ccc8e421cc5ba8194272af40c338c
kallebysantos <kalleby_santos@hotmail.com>
2023-07-12 20:47:07 +0000

    📦 NEW: Loading LLaMA model and processing user prompt

--- main.py
+++ main.py
@@ -1,15 +1,40 @@
 import datetime
-from time import time
 from flask import Flask, render_template, session
 from flask_socketio import SocketIO
+
+from langchain.llms import LlamaCpp
+from langchain import PromptTemplate, LLMChain
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+
+template = """Question: {question}
+
+Answer: Let's work this out in a step by step way to be sure we have the right answer."""
+
+prompt = PromptTemplate(template=template, input_variables=["question"])
+
+# Callbacks support token-wise streaming
+callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
+# Verbose is required to pass to the callback manager
+
+# Make sure the model path is correct for your system!
+llm = LlamaCpp(
+    model_path="./models/OpenLLaMA_3B.ggmlv1.q4_0.bin", callback_manager=callback_manager, verbose=True
+)
+
+llm_chain = LLMChain(prompt=prompt, llm=llm)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
 
 socket_io = SocketIO(app)
 
-def messageReceived(methods=['GET', 'POST']):
-    print('message was received!!!')
+def processPrompt(prompt: str):
+    socket_io.emit('response', {
+        'name': 'Assistant',
+        'message': llm_chain.run(prompt),
+        'timestamp': datetime.datetime.now().strftime('%m/%d/%Y, %H:%M:%S')
+    })
 
 @socket_io.on('connected')
 def handle_my_custom_event(json, methods=['GET', 'POST']):
@@ -19,12 +44,13 @@ def handle_my_custom_event(json, methods=['GET', 'POST']):
 
 @socket_io.on('message')
 def handle_my_custom_event(json, methods=['GET', 'POST']):
-    print('received my event: ' + str(json))
-    socket_io.emit('my response', json, callback=messageReceived)
+    message = json["data"]
+
+    processPrompt(message)
 
     return {
         'name': session["user"],
-        'message': json["data"],
+        'message': message,
         'timestamp': datetime.datetime.now().strftime('%m/%d/%Y, %H:%M:%S')
     }
 
