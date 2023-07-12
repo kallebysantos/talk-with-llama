6dd6d232b51413e6e491e5c1a001c2b84b6949bc
kallebysantos <kalleby_santos@hotmail.com>
2023-07-12 20:09:41 +0000

    📦 NEW: Receiving client messages

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+import datetime
+from time import time
+from flask import Flask, render_template, session
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
@@ -6,14 +8,25 @@ app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
 
 socket_io = SocketIO(app)
 
-
 def messageReceived(methods=['GET', 'POST']):
     print('message was received!!!')
 
-@socket_io.on('my event')
+@socket_io.on('connected')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    session["user"] = json["data"]["username"]
+
+    print('User connected: ' + session["user"])
+
+@socket_io.on('message')
 def handle_my_custom_event(json, methods=['GET', 'POST']):
     print('received my event: ' + str(json))
     socket_io.emit('my response', json, callback=messageReceived)
+
+    return {
+        'name': session["user"],
+        'message': json["data"],
+        'timestamp': datetime.datetime.now().strftime('%m/%d/%Y, %H:%M:%S')
+    }
 
 @app.route('/')
 def sessions():
